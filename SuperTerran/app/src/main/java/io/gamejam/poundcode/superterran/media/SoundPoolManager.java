10000020a0309447e08d382c57aa2bea9ab88aa1
chris_pound <c2.pound@gmail.com>
2015-08-08 16:44:56 -0500

    replaced deprecated call.

--- SuperTerran/app/src/main/java/io/gamejam/poundcode/superterran/media/SoundPoolManager.java
+++ SuperTerran/app/src/main/java/io/gamejam/poundcode/superterran/media/SoundPoolManager.java
@@ -1,9 +1,11 @@
 package io.gamejam.poundcode.superterran.media;
 
 import android.content.Context;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
+import android.os.Build;
 
 /**
  * Created by chris_pound on 8/8/15.
@@ -14,22 +16,28 @@ public class SoundPoolManager {
     private static SoundPool sp;
 
     private SoundPoolManager() {
-        sp = new SoundPool(4, 0,0);
-
     }
 
     public static void playAudio(int audiofileId, Context context) {
 
         if(sp == null) {
-            sp = new SoundPool(4, AudioManager.USE_DEFAULT_STREAM_TYPE, 0);
+            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                sp = new SoundPool(4, AudioManager.USE_DEFAULT_STREAM_TYPE, 0);
+            }
+            else {
+                AudioAttributes attributes = new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_GAME)
+                        .build();
+                sp = new SoundPool.Builder()
+                        .setMaxStreams(5)
+                        .setAudioAttributes(attributes)
+                        .build();
+            }
         }
-//      /** soundId for Later handling of sound pool **/
-        int soundId = sp.load(context, audiofileId, 1); // in 2nd param u have to pass your desire ringtone
-
+        int soundId = sp.load(context, audiofileId, 1);
         sp.play(soundId, 1, 1, 0, 0, 1);
+        MediaPlayer mPlayer = MediaPlayer.create(context, audiofileId);
 
-        MediaPlayer mPlayer = MediaPlayer.create(context, audiofileId); // in 2nd param u have to pass your desire ringtone
-        //mPlayer.prepare();
         mPlayer.start();
 
     }
