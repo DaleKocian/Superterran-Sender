37aec8341ea7dc5c15029669b9ad5e2fc0250e48
chris_pound <c2.pound@gmail.com>
2015-08-08 22:55:07 -0500

    added app id

--- SuperTerran/app/src/main/java/io/gamejam/poundcode/superterran/cast/CastConstants.java
+++ SuperTerran/app/src/main/java/io/gamejam/poundcode/superterran/cast/CastConstants.java
@@ -4,6 +4,6 @@ package io.gamejam.poundcode.superterran.cast;
  * Created by chris_pound on 8/8/15.
  */
 public class CastConstants {
-    public static final String CAST_APP_ID = "REPLACE WITH APP ID";
+    public static final String CAST_APP_ID = "E36862A2";
     public static final String CAST_CHANNEL_NAMESPACE = "urn:x-cast:io.poundcode.gamejam.superterran";
 }
